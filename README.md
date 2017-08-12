f55ea0150aa8ee04cb2b6288c5d9a54226a68dcc
talmeym <mtalmey@gmail.com>
2017-08-12 19:10:54 +0100

    worked on mq messaging, added log statements, added README content, updated create jms response to allow quickset of more params

--- README.md
+++ README.md
@@ -3,3 +3,29 @@
 regurgitator is a modular, light-weight, extendable java-based processing framework designed to 'regurgitate' canned or clever responses to incoming requests; useful for mocking or prototyping services.
 
 start your reading here: [regurgitator-all](http://github.com/talmeym/regurgitator-all#regurgitator)
+
+read more about regurgitator in json here: [regurgitator-core-json](http://github.com/talmeym/regurgitator-core-json#json-configuration-of-regurgitator)
+
+## extension mq steps in json
+
+### create-jms-response
+
+a create-jms-response step returns a response from regurgitator, allowing ``response-metadata`` parameters to be set for ``jms-destination``, ``correlation-id`` and ``jms-type``:
+
+```json
+{
+    "kind": "create-jms-response",
+    "name": "jms-response-creator",
+    "source": "response-param",
+    "jms-message-id": "messageid",
+    "jms-type": "my-jms-type",
+    "jms-destination: "queue://my-destination",
+    "jms-correlation-id": "52908d79-b7c6-4e18-ab13-e991d1b67d8b",
+    "jms-delivery-mode": 4,
+    "jms-expiration": "",
+    "jms-redelivered": true,
+    "jms-priority": "",
+    "jms-reply-to": "queue://other-destination",
+    "jms-timestamp: 123456789
+}
+```
--- src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseJsonLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseJsonLoader.java
@@ -1,7 +1,6 @@
 package com.emarte.regurgitator.extensions.mq;
 
 import com.emarte.regurgitator.core.*;
-import com.emarte.regurgitator.extensions.mq.CreateJmsResponse;
 import net.sf.json.JSONObject;
 
 import java.util.Set;
@@ -15,9 +14,16 @@ public class CreateJmsResponseJsonLoader implements JsonLoader<CreateJmsResponse
 	@Override
 	public CreateJmsResponse load(JSONObject jsonObject, Set<Object> allIds) throws RegurgitatorException {
 		CreateResponse response = (CreateResponse) responseJsonLoader.load(jsonObject, allIds);
+		String messageId = loadOptionalStr(jsonObject, JMS_MESSAGE_ID);
+		String type = loadOptionalStr(jsonObject, JMS_TYPE);
 		String destination = loadOptionalStr(jsonObject, JMS_DESTINATION);
 		String correlationId = loadOptionalStr(jsonObject, JMS_CORRELATION_ID);
-		String type = loadOptionalStr(jsonObject, JMS_TYPE);
-		return new CreateJmsResponse(response, destination, correlationId, type);
+		String deliveryMode = loadOptionalStr(jsonObject, JMS_DELIVERY_MODE);
+		String expiration = loadOptionalStr(jsonObject, JMS_EXPIRATION);
+		String priority = loadOptionalStr(jsonObject, JMS_PRIORITY);
+		String redelivered = loadOptionalStr(jsonObject, JMS_REDELiVERED);
+		String replyTo = loadOptionalStr(jsonObject, JMS_REPLY_TO);
+		String timestamp = loadOptionalStr(jsonObject, JMS_TIMESTAMP);
+		return new CreateJmsResponse(response, messageId, type, destination, correlationId, deliveryMode, expiration, priority, redelivered, replyTo, timestamp);
 	}
 }
