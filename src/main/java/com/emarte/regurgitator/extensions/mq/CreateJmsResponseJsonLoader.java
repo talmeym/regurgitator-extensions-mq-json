41a5f356f109c683b4c2766d97e8e1cff9ff53ec
talmeym <mtalmey@gmail.com>
2017-10-08 13:01:40 +0100

    Applied MIT License source comments

--- src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseJsonLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseJsonLoader.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
 package com.emarte.regurgitator.extensions.mq;
 
 import com.emarte.regurgitator.core.*;
@@ -9,21 +13,21 @@ import static com.emarte.regurgitator.core.JsonConfigUtil.loadOptionalStr;
 import static com.emarte.regurgitator.extensions.mq.ExtensionsMqConfigConstants.*;
 
 public class CreateJmsResponseJsonLoader implements JsonLoader<CreateJmsResponse> {
-	private CreateResponseJsonLoader responseJsonLoader = new CreateResponseJsonLoader();
+    private final CreateResponseJsonLoader responseJsonLoader = new CreateResponseJsonLoader();
 
-	@Override
-	public CreateJmsResponse load(JSONObject jsonObject, Set<Object> allIds) throws RegurgitatorException {
-		CreateResponse response = (CreateResponse) responseJsonLoader.load(jsonObject, allIds);
-		String messageId = loadOptionalStr(jsonObject, JMS_MESSAGE_ID);
-		String type = loadOptionalStr(jsonObject, JMS_TYPE);
-		String destination = loadOptionalStr(jsonObject, JMS_DESTINATION);
-		String correlationId = loadOptionalStr(jsonObject, JMS_CORRELATION_ID);
-		String deliveryMode = loadOptionalStr(jsonObject, JMS_DELIVERY_MODE);
-		String expiration = loadOptionalStr(jsonObject, JMS_EXPIRATION);
-		String priority = loadOptionalStr(jsonObject, JMS_PRIORITY);
-		String redelivered = loadOptionalStr(jsonObject, JMS_REDELiVERED);
-		String replyTo = loadOptionalStr(jsonObject, JMS_REPLY_TO);
-		String timestamp = loadOptionalStr(jsonObject, JMS_TIMESTAMP);
-		return new CreateJmsResponse(response, messageId, type, destination, correlationId, deliveryMode, expiration, priority, redelivered, replyTo, timestamp);
-	}
+    @Override
+    public CreateJmsResponse load(JSONObject jsonObject, Set<Object> allIds) throws RegurgitatorException {
+        CreateResponse response = (CreateResponse) responseJsonLoader.load(jsonObject, allIds);
+        String messageId = loadOptionalStr(jsonObject, JMS_MESSAGE_ID);
+        String type = loadOptionalStr(jsonObject, JMS_TYPE);
+        String destination = loadOptionalStr(jsonObject, JMS_DESTINATION);
+        String correlationId = loadOptionalStr(jsonObject, JMS_CORRELATION_ID);
+        String deliveryMode = loadOptionalStr(jsonObject, JMS_DELIVERY_MODE);
+        String expiration = loadOptionalStr(jsonObject, JMS_EXPIRATION);
+        String priority = loadOptionalStr(jsonObject, JMS_PRIORITY);
+        String redelivered = loadOptionalStr(jsonObject, JMS_REDELIVERED);
+        String replyTo = loadOptionalStr(jsonObject, JMS_REPLY_TO);
+        String timestamp = loadOptionalStr(jsonObject, JMS_TIMESTAMP);
+        return new CreateJmsResponse(response, messageId, type, destination, correlationId, deliveryMode, expiration, priority, redelivered, replyTo, timestamp);
+    }
 }
--- src/main/java/com/emarte/regurgitator/extensions/mq/ExtensionsMqJsonPackageMap.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/ExtensionsMqJsonPackageMap.java
@@ -1,13 +1,19 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
 package com.emarte.regurgitator.extensions.mq;
 
 import com.emarte.regurgitator.core.AbstractJsonPackageMap;
 
 import java.util.*;
 
-public class ExtensionsMqJsonPackageMap extends AbstractJsonPackageMap {
-	private static final List<String> kinds = Arrays.asList("create-jms-response");
+import static java.util.Collections.singletonList;
 
-	public ExtensionsMqJsonPackageMap() {
-		addPackageMapping(kinds, "com.emarte.regurgitator.extensions.mq");
-	}
+public class ExtensionsMqJsonPackageMap extends AbstractJsonPackageMap {
+    private static final List<String> kinds = singletonList("create-jms-response");
+
+    public ExtensionsMqJsonPackageMap() {
+        addPackageMapping(kinds, "com.emarte.regurgitator.extensions.mq");
+    }
 }
